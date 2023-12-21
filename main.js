f1148717ae7237798585c939cd0473f55f2711a7
saliims <saliim.domo@gmail.com>
2023-12-21 20:41:08 +0100

    file structure

---
+++ main.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow, Notification } = require("electron");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile("index.html");
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  setInterval(() => {
+    const notification = new Notification({
+      title: "Stay Hydrated!",
+      body: "It's time to drink water.",
+    });
+    notification.show();
+  }, 10000);
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
